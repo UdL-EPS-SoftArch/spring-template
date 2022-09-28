a9031fa3f825dbfbcc79bd267ee31d462be8fcbc
jnoguero <jrndll2@gmail.com>
2022-09-28 16:42:58 +0200

    create MessageEventHandle.java

---
+++ src/main/java/cat/udl/eps/softarch/demo/handler/MessageEventHandle.java
@@ -0,0 +1,44 @@
+package cat.udl.eps.softarch.demo.handler;
+
+
+import cat.udl.eps.softarch.demo.domain.Message;
+import cat.udl.eps.softarch.demo.repository.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler
+public class MessageEventHandle {
+    final Logger logger = LoggerFactory.getLogger(Message.class);
+
+    final MessageRepository messageRepository;
+
+    public MessageEventHandle(MessageRepository messageRepository){this.messageRepository = messageRepository;}
+
+    @HandleBeforeCreate
+    public void handleMessagePreCreate(Message message){logger.info("Before creating: {}", message.toString());}
+
+    @HandleBeforeSave
+    public void handleMessagePreSave(Message message){
+        logger.info("Before updating: {}", message.toString());
+    }
+    @HandleAfterCreate
+    public void handleMessagePostCreate(Message message) {
+        logger.info("After creating: {}", message.toString());
+        messageRepository.save(message);
+
+    }
+
+
+    @HandleAfterDelete
+    public void handleMessagePostDelete(Message message) {
+        logger.info("After deleting: {}", message.toString());
+    }
+
+    @HandleAfterLinkSave
+    public void handleMessagePostLinkSave(Message message, Object o) {
+        logger.info("After linking: {} to {}", message.toString(), o.toString());
+    }
+}
