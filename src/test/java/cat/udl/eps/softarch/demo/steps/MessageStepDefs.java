9aeff8775290c7d425b52d2c61b181514072c8bd
jnoguero <jrndll2@gmail.com>
2022-10-11 16:47:20 +0200

    Modify test MessageStepDefs.java and WebSecurityConfig.java

--- src/test/java/cat/udl/eps/softarch/demo/steps/MessageStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/MessageStepDefs.java
@@ -2,19 +2,30 @@ package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Message;
 import cat.udl.eps.softarch.demo.repository.MessageRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
 public class MessageStepDefs {
 
-    //private StepDefs stepDefs;
+    @Autowired
+    private StepDefs stepDefs;
     @Autowired
     private MessageRepository messageRepository;
+
+
 
 
     @Given("The message with id {long} doesn't exist")
@@ -25,14 +36,20 @@ public class MessageStepDefs {
     }
 
     @When("I send the message whit id {long} and text {string}")
-    public void iSendTheMessageWhitIdAndText(long ident, String text) {
+    public void iSendTheMessageWhitIdAndText(long ident, String text) throws Exception {
         ZonedDateTime date = ZonedDateTime.now();
-        if(!messageRepository.existsById(ident)){
-            Message message = new Message();
+        Message message = new Message();
+        message.setId(ident);
+        message.setWhen(date);
+        message.setText(text);
 
-            message.setWhen(date);
-            message.setText(text);
-            messageRepository.save(message);
-        }
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/messages")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stepDefs.mapper.writeValueAsString(message))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate())).andDo(print());
     }
+
+
 }
