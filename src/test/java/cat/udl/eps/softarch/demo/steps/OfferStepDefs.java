946abd5145984f01ebb5f88e5ec0d65e64e25f8a
catalin <cdojamihai@gmail.com>
2022-10-11 15:47:15 +0200

    Test of create almost done but there is a code 500 after the post.

--- src/test/java/cat/udl/eps/softarch/demo/steps/OfferStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/OfferStepDefs.java
@@ -7,13 +7,18 @@ import cat.udl.eps.softarch.demo.repository.UserRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class OfferStepDefs {
     @Autowired
@@ -55,7 +60,7 @@ public class OfferStepDefs {
 
     @Transactional
     @And("a ZoneDateTime {string} and a offerer user {string}")
-    public void aZoneDateTimeAndAOffererUser(String dateTime, String offerer) {
+    public void aZoneDateTimeAndAOffererUser(String dateTime, String offerer) throws Throwable {
         Date expectedDate = new Date("01/02/2018");
         offer.setDateTime(new Date(dateTime));
         Optional<User> user = userRepository.findById(offerer);
@@ -64,7 +69,14 @@ public class OfferStepDefs {
         Assert.assertEquals(expectedDate, offer.getDateTime());
         user.ifPresent(value -> Assert.assertEquals(value, offer.getOfferer()));
 
-        offerRepository.save(offer);
+        //offerRepository.save(offer);
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        post("/offers")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(stepDefs.mapper.writeValueAsString(offer)
+                                ).accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
     }
 
     @And("After all the steps I can retrieve this offer using the name {string} and the offererUser {string}")
