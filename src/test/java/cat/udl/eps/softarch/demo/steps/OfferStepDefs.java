39f242cc8949021ec5201564c666c7b6d1c76aa2
catalin <cdojamihai@gmail.com>
2022-10-13 18:21:11 +0200

    Offer CRUD tests done and working properly.

--- src/test/java/cat/udl/eps/softarch/demo/steps/OfferStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/OfferStepDefs.java
@@ -17,8 +17,14 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 public class OfferStepDefs {
     @Autowired
@@ -69,7 +75,6 @@ public class OfferStepDefs {
         Assert.assertEquals(expectedDate, offer.getDateTime());
         user.ifPresent(value -> Assert.assertEquals(value, offer.getOfferer()));
         System.out.println(offer.getDateTime());
-        offerRepository.save(offer);
 
         stepDefs.result = stepDefs.mockMvc.perform(
                         post("/offers")
@@ -80,8 +85,47 @@ public class OfferStepDefs {
                 .andDo(print());
     }
 
-    @And("After all the steps I can retrieve this offer using the name {string} and the offererUser {string}")
-    public void afterAllTheStepsICanRetrieveThisOfferUsingTheNameAndTheOffererUser(String name, String offererUser) {
+    @And("After all the steps I can retrieve this offer which should have the name {string}.")
+    public void afterAllTheStepsICanRetrieveThisOfferWhichShouldHaveTheName(String name) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        get("/offers/{id}", "1")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(status().isOk());
+    }
 
+
+
+    @Then("I want to modify this product's name to {string}.")
+    public void iWantToModifyThisProductSNameTo(String name) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        patch("/offers/{id}", "1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content((new JSONObject().put("name", name)).toString())
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate())
+                ).andDo(print())
+                .andExpect(jsonPath("$.name", is(name)));
+    }
+
+    @Then("I want to delete the offer with id {string}.")
+    public void iWantToDeleteTheOfferWithId(String id) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        delete("/offers/{id}", id)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    @And("I want to check that the offer doesn't exist anymore.")
+    public void iWantToCheckThatTheOfferDoesnTExistAnymore() throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        get("/offers/{id}", "1")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 }
