24f8f9e76fe2cdb04417406479972f43e1817bb3
Zihan Chen <zihan1048@live.com>
2024-03-11 12:11:43 +0100

    feat: create Provider entity

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Provider.java
@@ -0,0 +1,23 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import jakarta.persistence.Entity;
+import java.util.Collection;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Provider extends User {
+    @Override
+    @JsonValue(value = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_PROVIDER");
+    }
+}
