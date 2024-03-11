fb839e128f12daa0190de2e9e2050f09b165f876
Zihan Chen <zihan1048@live.com>
2024-03-11 12:13:03 +0100

    feat: create mapping controller and repository

---
+++ src/main/java/cat/udl/eps/softarch/demo/repository/MappingRepository.java
@@ -0,0 +1,24 @@
+package cat.udl.eps.softarch.demo.repository;
+
+import cat.udl.eps.softarch.demo.domain.Mapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryRestResource
+public interface MappingRepository extends PagingAndSortingRepository<Mapping, Long> {
+    List<Mapping> findByTitle(@Param("title") String title);
+    @Override
+    public Iterable<Mapping> findAll(Sort sort);
+
+    @Override
+    public Page<Mapping> findAll(Pageable pageable);
+
+    public Optional<Mapping> findById(Long id);
+}
