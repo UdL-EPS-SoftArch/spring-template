1c45a165bae2f868dcdee77dbb3275ac0fe6cbc7
Zihan Chen <zihan1048@live.com>
2024-03-14 13:46:53 +0100

    feat: Mapping Controller for POST method

--- src/main/java/cat/udl/eps/softarch/demo/controller/MappingController.java
+++ src/main/java/cat/udl/eps/softarch/demo/controller/MappingController.java
@@ -1,19 +1,18 @@
 package cat.udl.eps.softarch.demo.controller;
 import cat.udl.eps.softarch.demo.domain.Mapping;
+import cat.udl.eps.softarch.demo.domain.Supplier;
 import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.exception.NotAuthorizedException;
 import cat.udl.eps.softarch.demo.exception.NotFoundException;
 import cat.udl.eps.softarch.demo.repository.MappingRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -25,29 +24,46 @@ public class MappingController {
         this.mappingRepository = mappingRepository;
     }
 
-    @RequestMapping(value = "/mappings/{id}", method = RequestMethod.GET)
-    public @ResponseBody PersistentEntityResource getMapping(PersistentEntityResourceAssembler resourceAssembler,
-                                                             @PathVariable Long id) {
+//    @RequestMapping(value = "/mappings/{id}", method = RequestMethod.GET)
+//    public @ResponseBody PersistentEntityResource getMapping(PersistentEntityResourceAssembler resourceAssembler,
+//                                                             @PathVariable Long id) {
+//
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication instanceof AnonymousAuthenticationToken) {
+//            throw new NotAuthorizedException();
+//        }
+//
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        Optional<Mapping> mapping = mappingRepository.findById(id);
+//
+//        if (mapping.isEmpty()) {
+//            throw new NotFoundException();
+//        }
+//
+//        Mapping m = mapping.get();
+//
+//        if (m.getProvidedBy().getId().equals(user.getId())) {
+//            return resourceAssembler.toFullResource(m);
+//        } else {
+//            throw new NotAuthorizedException();
+//        }
+//    }
 
+    @RequestMapping(value = "/mappings", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody PersistentEntityResource createMapping(PersistentEntityResourceAssembler resourceAssembler,
+                                                                @RequestBody Mapping mapping) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication instanceof AnonymousAuthenticationToken) {
             throw new NotAuthorizedException();
         }
 
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Optional<Mapping> mapping = mappingRepository.findById(id);
+        Supplier supplier = (Supplier) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        if (mapping.isEmpty()) {
-            throw new NotFoundException();
-        }
+        mapping.setProvidedBy(supplier);
 
-        Mapping m = mapping.get();
-
-        if (m.getProvidedBy().getId().equals(user.getId())) {
-            return resourceAssembler.toFullResource(m);
-        } else {
-            throw new NotAuthorizedException();
-        }
+        mappingRepository.save(mapping);
+        return resourceAssembler.toFullResource(mapping);
     }
 
 }
